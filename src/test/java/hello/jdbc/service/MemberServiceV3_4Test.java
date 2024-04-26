120ec4f59bf300e8c8509ff4e12d4fc28c336959
null2p <pnr9255@gmail.com>
2024-04-26 22:43:22 +0900

    [feat] 스프링 부트를 이용한 datasource, transactionManager 자동 적용 테스트 코드 추가

---
+++ src/test/java/hello/jdbc/service/MemberServiceV3_4Test.java
@@ -0,0 +1,110 @@
+package hello.jdbc.service;
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.MemberRepositoryV3;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.support.AopUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+import static hello.jdbc.connection.ConnectionConst.*;
+
+@Slf4j
+@SpringBootTest
+class MemberServiceV3_4Test {
+
+    public static final String MemberA = "memberA";
+    public static final String MemberB = "memberB";
+    public static final String MemberEx = "ex";
+
+    @Autowired
+    private MemberRepositoryV3 memberRepository;
+    @Autowired
+    private MemberServiceV3_3 memberService;
+
+    @TestConfiguration
+    static class TestConfig {
+
+        private final DataSource dataSource;
+
+        public TestConfig(DataSource dataSource) {
+            this.dataSource = dataSource;
+        }
+
+        @Bean
+        MemberRepositoryV3 memberRepositoryV3() {
+            return new MemberRepositoryV3(dataSource);
+        }
+
+        @Bean
+        MemberServiceV3_3 memberServiceV3_3() {
+            return new MemberServiceV3_3(memberRepositoryV3());
+        }
+    }
+
+    @AfterEach
+    void after() throws SQLException {
+        memberRepository.delete(MemberA);
+        memberRepository.delete(MemberB);
+        memberRepository.delete(MemberEx);
+
+    }
+
+    @Test
+    void AopCheck() {
+        log.info("memberService class = {}", memberService.getClass());
+        log.info("memberRepository class = {}", memberRepository.getClass());
+
+        Assertions.assertThat(AopUtils.isAopProxy(memberService)).isTrue();
+        Assertions.assertThat(AopUtils.isAopProxy(memberRepository)).isFalse();
+    }
+
+    @Test
+    @DisplayName("정상 이체")
+    void accountTransfer() throws SQLException {
+        //given
+        Member memberA = new Member(MemberA, 10000);
+        Member memberB = new Member(MemberB, 10000);
+        memberRepository.save(memberA);
+        memberRepository.save(memberB);
+
+        //when
+        memberService.accountTransfer(memberA.getMemberId(), memberB.getMemberId(), 2000);
+
+        //then
+        Member foundMemberA = memberRepository.findById(memberA.getMemberId());
+        Member foundMemberB = memberRepository.findById(memberB.getMemberId());
+        Assertions.assertThat(foundMemberA.getMoney()).isEqualTo(8000);
+        Assertions.assertThat(foundMemberB.getMoney()).isEqualTo(12000);
+    }
+    @Test
+    @DisplayName("이체 중 예외 발생하지만 트랜잭션이 정상 작동하여 롤백된다")
+    void accountTransferEx() throws SQLException {
+        //given
+        Member memberA = new Member(MemberA, 10000);
+        Member memberEx = new Member(MemberEx, 10000);
+        memberRepository.save(memberA);
+        memberRepository.save(memberEx);
+        //when and then
+        Assertions.assertThatThrownBy(() -> memberService.accountTransfer(memberA.getMemberId(), memberEx.getMemberId(), 2000))
+                .isInstanceOf(IllegalStateException.class);
+
+
+        Member foundMemberA = memberRepository.findById(memberA.getMemberId());
+        Member foundMemberEx = memberRepository.findById(memberEx.getMemberId());
+        Assertions.assertThat(foundMemberA.getMoney()).isEqualTo(10000);
+        Assertions.assertThat(foundMemberEx.getMoney()).isEqualTo(10000);
+    }
+}
