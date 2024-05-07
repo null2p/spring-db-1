ac46aa5fae2647afe52c5a3123bd10481febeaed
null2p <pnr9255@gmail.com>
2024-05-07 20:16:12 +0900

    [feat] 기존 예외를 포함하는 스택 트레이스 로그 처리

--- src/test/java/hello/jdbc/exception/basic/UncheckedAppTest.java
+++ src/test/java/hello/jdbc/exception/basic/UncheckedAppTest.java
@@ -1,17 +1,28 @@
 package hello.jdbc.exception.basic;
 
+import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.net.ConnectException;
 import java.sql.SQLException;
 
+@Slf4j
 public class UncheckedAppTest {
 
     @Test
     void unchecked() {
         Controller controller = new Controller();
         Assertions.assertThatThrownBy(controller::request).isInstanceOf(RuntimeSQLException.class);
+    }
+
+    @Test
+    void printEx() {
+        Controller controller = new Controller();
+        try {
+            controller.request();
+        } catch (Exception e) {
+            log.info("ex", e);
+        }
     }
 
     static class Controller {
