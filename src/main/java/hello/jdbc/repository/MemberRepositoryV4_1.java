4883a1a7f7e793065359df663152992a883fb016
null2p <pnr9255@gmail.com>
2024-05-07 20:36:52 +0900

    [feat] MemberRepository 인터페이스화 및 JDBC 예외인 SQLException을 RuntimeException으로 전환

---
+++ src/main/java/hello/jdbc/repository/MemberRepositoryV4_1.java
@@ -0,0 +1,128 @@
+package hello.jdbc.repository;
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.ex.MyDbException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.datasource.DataSourceUtils;
+import org.springframework.jdbc.support.JdbcUtils;
+
+import javax.sql.DataSource;
+import java.sql.*;
+import java.util.NoSuchElementException;
+
+/**
+ * 예외 누수 문제 해결
+ * 체크 예외를 런타임 예외로 전환
+ * MemberRepository 인터페이스 활용
+ *  제거
+ */
+@Slf4j
+public class MemberRepositoryV4_1 implements MemberRepository {
+    private final DataSource dataSource;
+
+    public MemberRepositoryV4_1(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Override
+    public Member save(Member member) {
+        String sql = "INSERT INTO MEMBER(member_id, money) values (?,?)";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, member.getMemberId());
+            pstmt.setInt(2, member.getMoney());
+            pstmt.executeUpdate();
+            return member;
+        } catch (SQLException e) {
+            throw new MyDbException(e);
+        } finally {
+            close(con, pstmt, null);
+        }
+    }
+
+    @Override
+    public Member findById(String memberId)  {
+        String sql = "SELECT * FROM MEMBER WHERE member_id = ?";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, memberId);
+            rs = pstmt.executeQuery();
+            //한번은 호출 해야 내부 데이터 호출 가능
+            if (rs.next()) {
+                Member member = new Member();
+                member.setMemberId(rs.getString("member_id"));
+                member.setMoney(rs.getInt("money"));
+                return member;
+            } else {
+                throw new NoSuchElementException("멤버가 존재하지 않습니다! MEMBER ID = " + memberId);
+            }
+        } catch (SQLException e) {
+            throw new MyDbException(e);
+        } finally {
+            close(con, pstmt, rs);
+        }
+    }
+
+    @Override
+    public void update(String memberId, int money)  {
+        String sql = "update member set money=? where member_id=?";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setInt(1, money);
+            pstmt.setString(2, memberId);
+            int resultSize = pstmt.executeUpdate();
+            log.info("resultSize : {}",resultSize);
+        } catch (SQLException e) {
+            throw new MyDbException(e);
+        } finally {
+            close(con, pstmt, null);
+        }
+    }
+
+    @Override
+    public void delete(String memberId)  {
+        String sql = "delete from member where member_id = ?";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, memberId);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new MyDbException(e);
+        } finally {
+            close(con, pstmt, null);
+        }
+
+    }
+    private void close(Connection conn, Statement stmt, ResultSet resultSet) {
+        JdbcUtils.closeResultSet(resultSet);
+        JdbcUtils.closeStatement(stmt);
+        //주의! 트랜잭션 동기화를 사용하려면 DataSourceUtils 사용필요
+        DataSourceUtils.releaseConnection(conn,dataSource);
+    }
+    private Connection getConnection()  {
+        //주의! 트랜잭션 동기화를 사용하려면 DataSourceUtils 사용 필요
+        Connection connection = DataSourceUtils.getConnection(dataSource);
+        log.info("get CONNECTION = {}, class = {}",connection, connection.getClass());
+        return connection;
+    }
+}
---
+++ src/test/java/hello/jdbc/service/MemberServiceV4Test.java
@@ -0,0 +1,110 @@
+package hello.jdbc.service;
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.MemberRepository;
+import hello.jdbc.repository.MemberRepositoryV4_1;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.support.AopUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
+
+/**
+ * 예외 누수 문제 해결
+ * SQLException 제거
+ * MemberRepository 인터페이스 의존
+ */
+@Slf4j
+@SpringBootTest
+class MemberServiceV4Test {
+
+    public static final String MemberA = "memberA";
+    public static final String MemberB = "memberB";
+    public static final String MemberEx = "ex";
+
+    @Autowired
+    private MemberRepository memberRepository;
+    @Autowired
+    private MemberServiceV4 memberService;
+
+    @TestConfiguration
+    static class TestConfig {
+
+        private final DataSource dataSource;
+
+        public TestConfig(DataSource dataSource) {
+            this.dataSource = dataSource;
+        }
+
+        @Bean
+        MemberRepository memberRepository() {
+            return new MemberRepositoryV4_1(dataSource);
+        }
+
+        @Bean
+        MemberServiceV4 memberServiceV4() {
+            return new MemberServiceV4(memberRepository());
+        }
+    }
+
+    @AfterEach
+    void after() {
+        memberRepository.delete(MemberA);
+        memberRepository.delete(MemberB);
+        memberRepository.delete(MemberEx);
+
+    }
+
+    @Test
+    void AopCheck() {
+        log.info("memberService class = {}", memberService.getClass());
+        log.info("memberRepository class = {}", memberRepository.getClass());
+
+        Assertions.assertThat(AopUtils.isAopProxy(memberService)).isTrue();
+        Assertions.assertThat(AopUtils.isAopProxy(memberRepository)).isFalse();
+    }
+
+    @Test
+    @DisplayName("정상 이체")
+    void accountTransfer() {
+        //given
+        Member memberA = new Member(MemberA, 10000);
+        Member memberB = new Member(MemberB, 10000);
+        memberRepository.save(memberA);
+        memberRepository.save(memberB);
+
+        //when
+        memberService.accountTransfer(memberA.getMemberId(), memberB.getMemberId(), 2000);
+
+        //then
+        Member foundMemberA = memberRepository.findById(memberA.getMemberId());
+        Member foundMemberB = memberRepository.findById(memberB.getMemberId());
+        Assertions.assertThat(foundMemberA.getMoney()).isEqualTo(8000);
+        Assertions.assertThat(foundMemberB.getMoney()).isEqualTo(12000);
+    }
+    @Test
+    @DisplayName("이체 중 예외 발생하지만 트랜잭션이 정상 작동하여 롤백된다")
+    void accountTransferEx() {
+        //given
+        Member memberA = new Member(MemberA, 10000);
+        Member memberEx = new Member(MemberEx, 10000);
+        memberRepository.save(memberA);
+        memberRepository.save(memberEx);
+        //when and then
+        Assertions.assertThatThrownBy(() -> memberService.accountTransfer(memberA.getMemberId(), memberEx.getMemberId(), 2000))
+                .isInstanceOf(IllegalStateException.class);
+
+
+        Member foundMemberA = memberRepository.findById(memberA.getMemberId());
+        Member foundMemberEx = memberRepository.findById(memberEx.getMemberId());
+        Assertions.assertThat(foundMemberA.getMoney()).isEqualTo(10000);
+        Assertions.assertThat(foundMemberEx.getMoney()).isEqualTo(10000);
+    }
+}
